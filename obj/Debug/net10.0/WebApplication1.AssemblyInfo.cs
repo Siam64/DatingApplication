9bcd7017a6a1ed635940a2280ea57b7f7659d7aa
Samiul Alim <siamahmed64038@gmail.com>
2026-09-15 22:22:27 +0600

    appsettings

--- obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57c527bb66ef73907cadb8c2f6e11b3355a90391")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13b4427eeb4b5fe894c2d427984058136c8911fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
